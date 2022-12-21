b61b512020065fbe3e65ec2c00b1e03e2fa410df
승학산-08 <-08@DESKTOP-LF9QJHN>
2022-12-21 17:13:57 +0900

    TV solved

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,74 @@
+package tv;
+
+public class TV {
+	private int channel; // 1~255
+	private int volume; // 0~100
+	private boolean power;
+
+//	private void channel(int channel) {
+//		this.channel = channel;
+//	}
+//
+//	private void channel(Boolean up) {
+//	}
+//
+//	private void volume(int volume) {
+//		this.volume = volume;
+//	}
+//
+//	private void volume(Boolean up) {
+//	}
+
+	
+	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+
+	public void power(boolean on) {
+		this.power = on;
+	}
+
+	public void channel(boolean up) {
+		if (up) {
+			channel(channel + 1);
+		} else {
+			channel(channel - 1);
+		}
+	}
+
+	public void channel(int channel) {
+		if (channel < 1) {
+			channel = 255;
+		} else if (channel > 255) {
+			channel = 1;
+		}
+		this.channel = channel;
+	}
+
+	public void volume(boolean up) {
+		if (up) {
+			volume(volume + 1);
+		} else {
+			volume(volume - 1);
+		}
+	}
+
+	public void volume(int volume) {
+		if (volume < 0) {
+			volume = 100;
+		} else if (volume > 100) {
+			volume = 0;
+		}
+
+		this.volume = volume;
+	}
+
+	public void status() {
+		System.out.println("TV[power =" + (power ? "on" : "off") + ", " + "channel =" + channel + ", " + "volume ="
+				+ volume + "]");
+	}
+
+}
