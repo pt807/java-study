f626e82995961e69e4bddbd5c42b9456d4d86e6c
승학산-08 <-08@DESKTOP-LF9QJHN>
2022-12-21 17:13:27 +0900

    array solved

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -6,10 +6,28 @@ public class ArrayUtil {
 
 	public static double[] intToDouble(int[] is) {
 		double[] result = new double[is.length];
-		
-		for(int i = 0; i < is.length; i++) {
-		  result[i] = is[i];
+
+		for (int i = 0; i < is.length; i++) {
+			result[i] = is[i];
 		}
+		return result;
+	}
+
+	public static int[] doubleToInt(double[] is) {
+		int[] result = new int[is.length];
+
+		for (int i = 0; i < is.length; i++) {
+			result[i] = (int) is[i];
+		}
+		return result;
+	}
+
+	public static int[] concat(int[] is, int[] is2) {
+		int len = is.length + is2.length;
+		int[] result = new int[len];
+		
+		System.arraycopy(is, 0, result, 0, is.length);
+		System.arraycopy(is2, 0, result, is.length, is2.length);
 		return result;
 	}
 
