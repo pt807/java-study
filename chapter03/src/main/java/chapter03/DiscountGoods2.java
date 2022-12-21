52c4c4c603ce500204b646e18e1b9334cee9ce61
승학산-08 <-08@DESKTOP-LF9QJHN>
2022-12-21 09:55:34 +0900

    접근제어자

--- chapter03/src/main/java/chapter03/DiscountGoods2.java
+++ chapter03/src/main/java/chapter03/DiscountGoods2.java
@@ -3,5 +3,15 @@ package chapter03;
 import mypakage.Goods2;
 
 public class DiscountGoods2 extends Goods2 {
-
+	int i = 10;
+	//System.out.println(10); 리터널
+	//System.out.println(i);
+	//System.out.println(2+8);
+	
+	private float discountrate = 0.5f;
+	
+	public float getDiscountPrice() {
+		// protected는 자식에서 접근할 수 있다.
+		return discountrate * price;
+	}
 }
