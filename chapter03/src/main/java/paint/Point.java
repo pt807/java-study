48c6906fba545c9be001cdf5a48b84d0947d4fa7
pt807 <pt807@naver.com>
2022-12-21 19:46:22 +0900

    다형성 - overload

--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -4,6 +4,11 @@ public class Point {
 	private int x;
 	private int y;
 
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
 	public int getX() {
 		return x;
 	}
@@ -22,6 +27,15 @@ public class Point {
 
 	public void show() {
 		System.out.println("점(x=" + this.x + ", y=" + this.y + ")을 그렸습니다.");
+	}
+
+	public void show(boolean visible) {
+		if (visible) {
+			show(); // 코드 중복 방지
+		} else {
+			System.out.println("점(x=" + this.x + ", y=" + this.y + ")을 지웠습니다.");
+
+		}
 	}
 
 }
