c3c272a548eec33ff6e0100ecd745e21f306c80d
pt807 <pt807@naver.com>
2022-12-23 16:05:34 +0900

    String #2

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,29 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+
+		// "HELLO"
+		System.out.println("\"HELLO\"");
+		
+		// \t : tab
+		// \r : carriage return
+		// \n : newline
+		// \b : bell
+
+		System.out.print("hello\tworld\n");
+		System.out.println("hello\tworld");
+		
+		//'
+		//char c = '\';
+		
+		
+		System.out.print(1 + "\n");
+		System.out.print(2);
+
+	}
+
+}
