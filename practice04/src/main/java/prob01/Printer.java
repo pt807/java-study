3a7b2c8c80b0f84d8aaec4430371eb7e6efc4d81
pt807 <pt807@naver.com>
2022-12-26 09:54:47 +0900

    Generic Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,22 +2,42 @@ package prob01;
 
 public class Printer {
 
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//
+//	}
+//
+//	public void println(String s) {
+//		System.out.println(s);
+//
+//	}
+
+	// ex) extends number 제한가능
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 
-	public void println(boolean b) {
-		System.out.println(b);
-
+	// 가변 파라미터
+	public int sum(Integer... nums) {
+		int s = 0;
+		for (Integer n : nums) {
+			s += n;
+		}
+		return s;
 	}
 
-	public void println(double d) {
-		System.out.println(d);
-
-	}
-
-	public void println(String s) {
-		System.out.println(s);
-
+	@SuppressWarnings("unchecked")
+	public <T> void println(T... ts) {
+		for (T t : ts)
+			System.out.println(t);
 	}
 }
