403df964644e30bfb95745f0bcc74f9b07a95986
pt807 <pt807@naver.com>
2022-12-28 16:03:19 +0900

    TCPServer #1

---
+++ network/src/main/java/util/NSLookup.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class NSLookup {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			while (true) {
+				System.out.print(">");
+				String line = sc.nextLine();
+				if (line.equals("exit")) {
+					sc.close();
+					break;
+				}
+				InetAddress[] inetAddresses = InetAddress.getAllByName(line);
+				for (InetAddress inetAddress : inetAddresses) {
+					System.out.println(line + " : " + inetAddress.getHostAddress());
+				}
+			}
+
+		} catch (UnknownHostException e) {
+			System.out.println("error");
+		}
+	}
+
+}
