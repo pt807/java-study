2b6710ee0735dbbc5bc0490c51b1def0604e4405
pt807 <pt807@naver.com>
2022-12-29 16:39:54 +0900

    httpd #finish

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -1,13 +1,16 @@
 package httpd;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENT_ROOT = "./webapp";
 	private Socket socket;
 
 	public RequestHandler(Socket socket) {
@@ -26,6 +29,7 @@ public class RequestHandler extends Thread {
 			consoleLog("connected from " + inetSocketAddress.getAddress().getHostAddress() + ":"
 					+ inetSocketAddress.getPort());
 
+			String request = null;
 			while (true) {
 				String line = br.readLine();
 
@@ -34,20 +38,38 @@ public class RequestHandler extends Thread {
 					break;
 				}
 
-				// SimpleHttpServer는 요처의 헤더만 처리한다.
+				// SimpleHttpServer는 요청의 헤더만 처리한다.
 				if ("".equals(line)) {
 					break;
 				}
 
-				consoleLog(line);
+				// 요청 헤더의 첫 번째 라인만 읽음
+				if (request == null) {
+					request = line;
+					break;
+				}
+			}
+
+			// 요청 처리
+			consoleLog(request);
+
+			String[] tokens = request.split(" ");
+			if ("GET".equals(tokens[0])) {
+				reponseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else {
+				// methods: POST, PUT, DELETE, HEAD, CONNECT
+				// SimpleHttpServer 에서는 무시(400 Bad Resquest)
+				reponse400Error(outputStream, tokens[2]);
 			}
 
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-			outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
-			outputStream.write("Content-Type:text/html; charset=utf-8\r\n".getBytes("UTF-8"));
-			outputStream.write("\r\n".getBytes());
-			outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes("UTF-8"));
+			// outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
+			// outputStream.write("Content-Type:text/html;
+			// charset=utf-8\r\n".getBytes("UTF-8"));
+			// outputStream.write("\r\n".getBytes() );
+			// outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된
+			// 것입니다.</h1>".getBytes( "UTF-8" ) );
 
 		} catch (Exception ex) {
 			consoleLog("error:" + ex);
@@ -62,6 +84,62 @@ public class RequestHandler extends Thread {
 				consoleLog("error:" + ex);
 			}
 		}
+	}
+
+	private void reponseStaticResource(OutputStream outputStream, String url, String protocol) throws IOException {
+
+		// default(welcome) file set
+		if ("/".equals(url)) {
+			url = "/index.html";
+		}
+
+		File file = new File(DOCUMENT_ROOT + url);
+		if (!file.exists()) {
+			reponse404Error(outputStream, protocol);
+			return;
+		}
+
+		// nio
+		// 파일의 모든 줄을 읽기
+		byte[] body = Files.readAllBytes(file.toPath());
+		// mimeType 반환
+		String contentType = Files.probeContentType(file.toPath());
+
+		// 응답
+		outputStream.write((protocol + " 200 OK\r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
+	}
+
+	private void reponse404Error(OutputStream outputStream, String protocol) throws IOException {
+		File file = new File(DOCUMENT_ROOT + "/error/404.html");
+		if (file.exists() == false) {
+			System.out.println("file not found: " + file.getPath());
+			return;
+		}
+
+		byte[] body = Files.readAllBytes(file.toPath());
+
+		String contentType = Files.probeContentType(file.toPath());
+
+		outputStream.write((protocol + " 404 Bad Request\r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
+	}
+
+	private void reponse400Error(OutputStream outputStream, String string) throws IOException {
+		File file = new File(DOCUMENT_ROOT + "/error/400.html");
+
+		byte[] body = Files.readAllBytes(file.toPath());
+
+		String contentType = Files.probeContentType(file.toPath());
+
+		outputStream.write("HTTP/1.1 400 Bad Request\r\n".getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
 	}
 
 	public void consoleLog(String message) {
