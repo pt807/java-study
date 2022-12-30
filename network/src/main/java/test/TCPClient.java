e01c2c85f1efd48101215823c7df2f8d227360c8
pt807 <pt807@naver.com>
2022-12-30 13:55:46 +0900

    Socket Options #finisih

--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class TCPClient {
 	private static final String SERVER_IP = "127.0.0.1";
@@ -14,10 +15,29 @@ public class TCPClient {
 	public static void main(String[] args) {
 		Socket socket = null;
 		try {
-			// 1.소켓 생성
+			// 1. 소켓 생성
 			socket = new Socket();
 
-			// 2.서버 연결
+			// 1-1. 소켓 버퍼 사이즈 확인
+			int rcvBufferSize = socket.getReceiveBufferSize();
+			int sndBufferSize = socket.getSendBufferSize();
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+
+			// 1-2 소켓 버퍼 사이즈 변경
+			socket.setReceiveBufferSize(1024 * 10);
+			socket.setSendBufferSize(1024 * 10);
+
+			rcvBufferSize = socket.getReceiveBufferSize();
+			sndBufferSize = socket.getSendBufferSize();
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+
+			// 1-3. SO_NODELAY(Nagle Algorithm off)
+			socket.setTcpNoDelay(true);
+
+			// 1-4. SO_TIMEOUT
+			socket.setSoTimeout(3000);
+
+			// 2. 서버 연결
 			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
 
 			// 3. IO Stream 받아오기
@@ -25,7 +45,7 @@ public class TCPClient {
 			OutputStream os = socket.getOutputStream();
 
 			// 4. 쓰기
-			String data = "hello world";
+			String data = "Hello World";
 			os.write(data.getBytes("utf-8"));
 
 			// 5. 읽기
@@ -38,12 +58,13 @@ public class TCPClient {
 			}
 
 			data = new String(buffer, 0, readByteCount, "utf-8");
-			System.out.println("[client] received: " + data);
-
+			System.out.println("[client] received:" + data);
+		} catch (SocketTimeoutException ex) {
+			System.out.println("[client] timeout!!!!!!");
 		} catch (SocketException ex) {
-			System.out.println("[server] suddenly closed by server");
+			System.out.println("[clinet] suddenly closed by server");
 		} catch (IOException e) {
-			System.out.println("[client] error: " + e);
+			System.out.println("[client] error:" + e);
 		} finally {
 			try {
 				if (socket != null && !socket.isClosed()) {
@@ -54,5 +75,4 @@ public class TCPClient {
 			}
 		}
 	}
-
 }
--- network/src/main/java/test/TCPserver.java
+++ network/src/main/java/test/TCPserver.java
@@ -3,23 +3,24 @@ package test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
 public class TCPserver {
-
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
 		try {
-			// 1. server soket 생성
+			// 1. Server Socket 생성
 			serverSocket = new ServerSocket();
 
-			// 2. binding(바인딩)
-			// soket에 InetSoketaddress(IPAddress + port)를 바인딩 한다.
-			// IPAdress: 0.0.0.0: 특정 호스트 iP에 바인딩 하지 않는다.
+			// 1-1. FIN_WAIT -> TIME_WAIT 상태에서도 소켓 포트 할당이 가능하도록 하기 위해
+			serverSocket.setReuseAddress(true);
+
+			// 2. 바인딩(binding)
+			// Socket에 InetSocketaddress(IPAddress + Port)를 바인딩 한다.
+			// IPAddress: 0.0.0.0: 특정 호스트 IP에 바인딩 하지 않는다.
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000), 10);
 
 			// 3. accept
@@ -40,13 +41,20 @@ public class TCPserver {
 					byte[] buffer = new byte[256];
 					int readByteCount = is.read(buffer); // blocking
 					if (readByteCount == -1) {
-						// 서버가 정상적으로 종료(close() 호출)
+						// 클라이언트가 정상적으로 종료(close() 호출)
 						System.out.println("[server] closed by client");
 						break;
 					}
 
 					String data = new String(buffer, 0, readByteCount, "utf-8");
-					System.out.println("[server] received: " + data);
+					System.out.println("[server] received:" + data);
+
+					// SO_TIMEOUT 옵션 테스트용
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 
 					// 6. 데이터 쓰기
 					os.write(data.getBytes("utf-8"));
@@ -54,7 +62,7 @@ public class TCPserver {
 			} catch (SocketException ex) {
 				System.out.println("[server] suddenly closed by client");
 			} catch (IOException ex) {
-				System.out.println("[server] error: " + ex);
+				System.out.println("[server] error:" + ex);
 			} finally {
 				try {
 					if (socket != null && !socket.isClosed()) {
@@ -64,8 +72,9 @@ public class TCPserver {
 					ex.printStackTrace();
 				}
 			}
+
 		} catch (IOException e) {
-			System.out.println("[server] error: " + e);
+			System.out.println("[server] error:" + e);
 		} finally {
 			try {
 				if (serverSocket != null && !serverSocket.isClosed()) {
@@ -76,5 +85,4 @@ public class TCPserver {
 			}
 		}
 	}
-
 }
